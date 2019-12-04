649fd7c563fa97beff0756478fb5f5454e661993
Thordreck <paezguerraalvaro@gmail.com>
2019-12-04 13:06:32 +0100

    Removed unused actionlib types conversion functions.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -13,7 +13,6 @@
 #include "behavior_tree_ros/decorators/ForEachLoopNode.hpp"
 #include "behavior_tree_ros/actions/ConvertMessageFieldNode.hpp"
 #include "behavior_tree_ros/actions/ConvertRandomMessageFieldNode.hpp"
-//#include "behavior_tree_ros/SimpleActionClientNode.hpp"
 
 namespace BT_ROS
 {
@@ -21,13 +20,6 @@ namespace BT_ROS
     {
         return _input.dump();
     }
-
-    /*
-    std::string goalState2String(const actionlib::SimpleClientGoalSTate& _state)
-    {
-        _state.toString();
-    }
-    */
 }
 
 BT_REGISTER_NODES(factory)
@@ -56,5 +48,4 @@ BT_REGISTER_NODES(factory)
 
     factory.registerTypeConverter<std::string, nlohmann::json>(BT::convertFromString<nlohmann::json>);
     factory.registerTypeConverter<nlohmann::json, std::string>(json2String);
-    //factory.registerTypeConverter<actionlib::SimpleClientGoalState, std::string>(goalState2String);
 }
