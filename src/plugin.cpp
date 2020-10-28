d60447627d51407e61b651a8ef9baa251941d982
Piruri <javichulani@gmail.com>
2020-10-28 10:57:40 +0100

    Plugin: Added GetTransformAnglesNode from behavior_tree_extensions plugin

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -28,6 +28,7 @@
 #include "behavior_tree_ros/actions/GetTransformDistanceNode.hpp"
 #include "behavior_tree_ros/actions/GetTransformHorizontalDistanceNode.hpp"
 #include "behavior_tree_ros/actions/GetTransformOriginNode.hpp"
+#include "behavior_tree_ros/actions/GetTransformAnglesNode.hpp"
 #include "behavior_tree_ros/actions/LoadYamlFileNode.hpp"
 
 #include "behavior_tree_ros/PublisherNode.hpp"
@@ -75,6 +76,7 @@ BT_REGISTER_NODES(factory)
     factory.registerNodeType<GetTransformDistanceNode>("GetTransformDistance");
     factory.registerNodeType<GetTransformHorizontalDistanceNode>("GetTransformHorizontalDistance");
     factory.registerNodeType<GetTransformOriginNode>("GetTransformOrigin");
+    factory.registerNodeType<GetTransformAnglesNode>("GetTransformAngles");
     factory.registerNodeType<LoadYamlFileNode>("LoadYamlFile");
 
     factory.registerNodeType<SerializedSubscriber<std_msgs::Int32>>("MonitorStdInt32");
