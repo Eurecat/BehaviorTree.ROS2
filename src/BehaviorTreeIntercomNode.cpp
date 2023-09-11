4771ce54813f3e85ec260fdeeb528e1f66cdc6e2
Eric Nichols <e.nichols@jp.honda-ri.com>
2023-09-11 12:58:23 +0200

    sending sync 5 times

--- src/BehaviorTreeIntercomNode.cpp
+++ src/BehaviorTreeIntercomNode.cpp
@@ -6,41 +6,32 @@ namespace BT_ROS
         handshake_action_server_ (public_node_handle_, "behavior_tree/handshake", boost::bind(&RosHandShake::HandShakeActionCallback, this, _1), false)
     {
         // Get intercom topic name
-        std::string handshake_topic_name = private_node_handle_.param<std::string>("handshake_topic_name", "/remote/bt_handshake");
-
-        // Publisher
-        send_signal_publisher_ = public_node_handle_.advertise<std_msgs::String>(handshake_topic_name, 1, true);
-
-        // Subscriber
-        get_signal_subscriber_ = public_node_handle_.subscribe(handshake_topic_name, 10, &RosHandShake::HandShakeTopicCallback, this);
-
-        // Actionlib
+        std::string handshake_topic_name = private_node_handle_.param<std::string>("handshake_topic_name", "/remote/bt_handshake");        // Publisher
+        send_signal_publisher_ = public_node_handle_.advertise<std_msgs::String>(handshake_topic_name, 1, true);        // Subscriber
+        get_signal_subscriber_ = public_node_handle_.subscribe(handshake_topic_name, 10, &RosHandShake::HandShakeTopicCallback, this);        // Actionlib
         handshake_action_server_.registerPreemptCallback(boost::bind(&RosHandShake::HandShakeActionPreemptCallback, this));
         handshake_action_server_.start();
-    }
-
-    void RosHandShake::HandShakeTopicCallback(const std_msgs::StringConstPtr& _topic_msg)
+    }    void RosHandShake::HandShakeTopicCallback(const std_msgs::StringConstPtr& _topic_msg)
     {
         ROS_INFO("Received Handshake topic message! [%s]", _topic_msg->data.c_str());
         std::unique_lock<std::mutex> lock (handshake_mutex_);
         handshake_topic_msgs_.emplace_back(_topic_msg->data);
         new_handshake_topic_msg_ = true;
-    }
-
-    void RosHandShake::HandShakeActionCallback(const behavior_tree_ros::HandShakeGoalConstPtr& _goal_msg)
+    }    void RosHandShake::HandShakeActionCallback(const behavior_tree_ros::HandShakeGoalConstPtr& _goal_msg)
     {
         ROS_INFO("Starting Handshake Action callback! [%s] [%s]", _goal_msg->bt_id.c_str(), _goal_msg->message.c_str());
         bool signal_sent = false;
         bool signal_received = false;
-
+        int  time_count = 0;        
+        
         try
         {
             while(!signal_received && ros::ok() && handshake_action_server_.isActive())
             {
                 // Check signal received is the correct one
                 {
-                    std::unique_lock<std::mutex> lock (handshake_mutex_);
-
+                    std::unique_lock<std::mutex> lock (handshake_mutex_);                    
+                    
                     if(new_handshake_topic_msg_)
                     {
                         new_handshake_topic_msg_ = false;
@@ -58,36 +49,36 @@ namespace BT_ROS
                                 ROS_INFO("Message received from another node [%s]", it->c_str());
                                 // Remove matched message
                                 handshake_topic_msgs_.erase(it--);
-                                signal_received = true;
-
+                                signal_received = true;                                
                                 break; // Stop checking msgs as we found what we were looking for
                             }
                             // Remove unmatched message
                             handshake_topic_msgs_.erase(it--);
                         }
                     }
-                }
-
+                }               
                 if(!signal_sent){
                     // Send a signal every time
                     ROS_INFO("Sending signal to the other node!");
                     std_msgs::String msg_to_send;
                     msg_to_send.data = _goal_msg->bt_id + ":" + _goal_msg->message;
-                    send_signal_publisher_.publish(msg_to_send);
-                    usleep(2e5);
-                    send_signal_publisher_.publish(msg_to_send);
-                    usleep(2e5);
-                    send_signal_publisher_.publish(msg_to_send);
-                    usleep(2e5);
-                    send_signal_publisher_.publish(msg_to_send);
-                    usleep(2e5);
-                    send_signal_publisher_.publish(msg_to_send);
+                    for(int i = 0; i<=7; i++)
+                    {
+                        send_signal_publisher_.publish(msg_to_send);
+                        usleep(2e5);
+                    }
                     signal_sent = true;
+                }                
+                
+
+                usleep(2e5); // not overload CPU                
+                
+                time_count++;                
+                
+                if(time_count == 25){
+                    signal_sent = false;
                 }
-
-                usleep(2e5); // not overload CPU
-            }
-
+            }            
             if(handshake_action_server_.isActive())
             {
                 ROS_INFO("Handshake succeeded!!");
@@ -101,57 +92,37 @@ namespace BT_ROS
             handshake_action_result_.result = false;
             handshake_action_server_.setAborted(handshake_action_result_, "Synchronization aborted!");
         }
-    }
-
-    void RosHandShake::HandShakeActionPreemptCallback()
+    }    void RosHandShake::HandShakeActionPreemptCallback()
     {
         ROS_INFO("Handshake Action Goal canceled!");
         handshake_action_result_.result = false;
         handshake_action_server_.setPreempted(handshake_action_result_, "Goal preempted");
-    }
-
-    RosExchangeInfo::RosExchangeInfo() :
+    }    RosExchangeInfo::RosExchangeInfo() :
         exchange_info_action_server_ (public_node_handle_, "behavior_tree/exchange_info", boost::bind(&RosExchangeInfo::ExchangeInfoActionCallback, this, _1), false)
     {
         // Get intercom topic name
-        std::string exchange_info_topic_name = private_node_handle_.param<std::string>("exhange_info_topic_name", "/remote/bt_info");
-
-        // Publisher
-        send_info_publisher_ = public_node_handle_.advertise<std_msgs::String>(exchange_info_topic_name, 1, true);
-
-        // Subscriber
-        get_info_subscriber_ = public_node_handle_.subscribe(exchange_info_topic_name, 10, &RosExchangeInfo::ExchangeInfoTopicCallback, this);
-
-        // Actionlib
+        std::string exchange_info_topic_name = private_node_handle_.param<std::string>("exhange_info_topic_name", "/remote/bt_info");        // Publisher
+        send_info_publisher_ = public_node_handle_.advertise<std_msgs::String>(exchange_info_topic_name, 1, true);        // Subscriber
+        get_info_subscriber_ = public_node_handle_.subscribe(exchange_info_topic_name, 10, &RosExchangeInfo::ExchangeInfoTopicCallback, this);        // Actionlib
         exchange_info_action_server_.registerPreemptCallback(boost::bind(&RosExchangeInfo::ExchangeInfoActionPreemptCallback, this));
         exchange_info_action_server_.start();
-    }
-
-
-    void RosExchangeInfo::ExchangeInfoTopicCallback(const std_msgs::StringConstPtr& _topic_msg)
+    }    void RosExchangeInfo::ExchangeInfoTopicCallback(const std_msgs::StringConstPtr& _topic_msg)
     {
         ROS_INFO("Received Info topic message! [%s]", _topic_msg->data.c_str());
         std::unique_lock<std::mutex> lock (exchange_info_mutex_);
         exchange_info_topic_msgs_.emplace_back(_topic_msg->data);
         new_exchange_info_topic_msg_ = true;
-    }
-
-
-    void RosExchangeInfo::ExchangeInfoActionCallback(const behavior_tree_ros::ExchangeInfoGoalConstPtr& _goal_msg)
+    }    void RosExchangeInfo::ExchangeInfoActionCallback(const behavior_tree_ros::ExchangeInfoGoalConstPtr& _goal_msg)
     {
         ROS_INFO("Starting ExchangeInfo Action callback! [%s] [%s] [%s]", _goal_msg->bt_id.c_str(), _goal_msg->info_type.c_str(), _goal_msg->info_data.c_str());
         bool signal_sent = false;
-        bool signal_received = false;
-
-        try
+        bool signal_received = false;        try
         {
             while(!signal_received && ros::ok() && exchange_info_action_server_.isActive())
             {
                 // Check signal received is the correct one
                 {
-                    std::unique_lock<std::mutex> lock (exchange_info_mutex_);
-
-                    if(new_exchange_info_topic_msg_)
+                    std::unique_lock<std::mutex> lock (exchange_info_mutex_);                    if(new_exchange_info_topic_msg_)
                     {
                         new_exchange_info_topic_msg_ = false;
                         std::string exchange_info_bt_id {""};
@@ -164,30 +135,22 @@ namespace BT_ROS
                             size_t pos2 = it->find_last_of(":");
                             exchange_info_bt_id = it->substr(0, pos);
                             exchange_info_type = it->substr(pos+1, pos2-pos-1);
-                            exchange_info_data = it->substr(pos2+1, it->size());
-
-                            ROS_INFO("EX Message received from another node [%s] [%s] [%s]", exchange_info_bt_id.c_str(), exchange_info_type.c_str(), exchange_info_data.c_str());
+                            exchange_info_data = it->substr(pos2+1, it->size());                            ROS_INFO("EX Message received from another node [%s] [%s] [%s]", exchange_info_bt_id.c_str(), exchange_info_type.c_str(), exchange_info_data.c_str());
                             // Checking if msg received is not empty, is from another BT and is in the same stage
                             if(!it->empty() && exchange_info_bt_id != _goal_msg->bt_id && exchange_info_type == _goal_msg->info_type)
                             {
                                 ROS_INFO("Message received from another node [%s]", it->c_str());
                                 // Remove matched message
                                 exchange_info_topic_msgs_.erase(it--);
-                                signal_received = true;
-
-                                // Fill result message
+                                signal_received = true;                                // Fill result message
                                 exchange_info_action_result_.info_type = exchange_info_type;
-                                exchange_info_action_result_.info_data = exchange_info_data;
-
-                                break; // Stop checking msgs as we found what we were looking for
+                                exchange_info_action_result_.info_data = exchange_info_data;                                break; // Stop checking msgs as we found what we were looking for
                             }
                             // Remove unmatched message
                             exchange_info_topic_msgs_.erase(it--);
                         }
                     }
-                }
-
-                // Send signal only one time
+                }                // Send signal only one time
                 if(!signal_sent)
                 {
                     ROS_INFO("Sending signal to the other node!");
@@ -195,12 +158,8 @@ namespace BT_ROS
                     msg_to_send.data = _goal_msg->bt_id + ":" + _goal_msg->info_type + ":" + _goal_msg->info_data;
                     send_info_publisher_.publish(msg_to_send);
                     signal_sent = true;
-                }
-
-                usleep(2e5); // not overload CPU
-            }
-
-            if(exchange_info_action_server_.isActive())
+                }                usleep(2e5); // not overload CPU
+            }            if(exchange_info_action_server_.isActive())
             {
                 ROS_INFO("ExchangeInfo succeeded!!");
                 exchange_info_action_result_.info_received = true;
@@ -213,12 +172,10 @@ namespace BT_ROS
             exchange_info_action_result_.info_received = false;
             exchange_info_action_server_.setAborted(exchange_info_action_result_, "Synchronization aborted!");
         }
-    }
-
-    void RosExchangeInfo::ExchangeInfoActionPreemptCallback()
+    }    void RosExchangeInfo::ExchangeInfoActionPreemptCallback()
     {
         ROS_INFO("ExchangeInfo Action Goal canceled!");
         exchange_info_action_result_.info_received = false;
         exchange_info_action_server_.setPreempted(exchange_info_action_result_, "Goal preempted");
-    }
-} // namespace BT_ROS
+}
+}// namespace BT_ROS
