c389ed30905eaaa5716ce1f873e1b365cd5dd5fd
David Calero <david.calero@eurecat.org>
2024-01-16 15:16:53 +0100

    Bug fixed on ROS:ERROR message

--- include/behavior_tree_ros/actions/LoadYamlFileNode.hpp
+++ include/behavior_tree_ros/actions/LoadYamlFileNode.hpp
@@ -42,7 +42,7 @@ class LoadYamlFileNode final : public BT::SyncActionNode
                 std::string ros_pkg_path = ros::package::getPath(package_name);
                 if (ros_pkg_path == "")
                 {
-                    ROS_ERROR("Error loading tree %s: Package %s not found.", action_tree_.tree_UID_, full_path.c_str(), ex.what());
+                    ROS_ERROR("Error: Package %s not found.", package_name.c_str());
                 }
                 else
                     absolute_file_path = ros_pkg_path + package_relative_path;
