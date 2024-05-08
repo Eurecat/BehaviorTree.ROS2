ca6ff03f3b40d38dad471deea0be4fcdf21d97a7
Devis <devis.dalmoro@studenti.unitn.it>
2024-05-08 12:20:43 +0900

    Multiple yaml files + multiple key-value instring replacements
    - Modified format of the srv and action msg to access a vector of string for the yaml filepaths
    - Replace different keys with strict syntax ${} in a single string value for the init BB

--- src/BehaviorTreeNode.cpp
+++ src/BehaviorTreeNode.cpp
@@ -137,7 +137,7 @@ namespace BT_ROS
             // the full path to be consistent with the original request.
             current_tree_ = _request.tree_file;
 
-            service_tree_.BuildTree(full_path, bt_factory_, _request.debug, _request.bb_init_file);
+            service_tree_.BuildTree(full_path, bt_factory_, _request.debug, _request.bb_init_files);
             service_tree_.InitializeLoggers(enable_cout_log_, enable_minitrace_log_, enable_file_log_, enable_rostopic_log_, enable_zmq_log_, log_folder_);
 
             ROS_INFO("Loaded tree %s", full_path.c_str());
@@ -391,7 +391,12 @@ namespace BT_ROS
         try
         {
             ROS_INFO("Loading action tree %s", full_path.c_str());
-            action_tree_.BuildTree(full_path, bt_factory_, goal->debug, goal->bb_init_file.data);
+            std::vector<std::string> initbb_yaml_filepaths{};
+            for(auto initbbyaml_path_it = goal->bb_init_files.begin(); initbbyaml_path_it != goal->bb_init_files.end(); initbbyaml_path_it++)
+            {
+                initbb_yaml_filepaths.push_back(std::string{initbbyaml_path_it->data});
+            }
+            action_tree_.BuildTree(full_path, bt_factory_, goal->debug, initbb_yaml_filepaths);
 
             // If the service tree is loaded it means that the action was called from the remote BT block
             // As such, don't show status messages through the terminal
