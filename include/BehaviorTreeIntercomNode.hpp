08df829208c4797443465f7cdb1d0b50ea750423
David Calero <david.calero@eurecat.org>
2024-06-20 18:12:52 +0200

    Removed 3wayHandShakeData from hpp definition too

--- include/BehaviorTreeIntercomNode.hpp
+++ include/BehaviorTreeIntercomNode.hpp
@@ -13,9 +13,7 @@
 #include <std_msgs/Bool.h>
 
 #include "behavior_tree_ros/HandShake.h"
-#include "behavior_tree_ros/HandShakeData.h"
 #include "behavior_tree_ros/PerformHandShakeAction.h"
-#include "behavior_tree_ros/PerformHandShakeDataAction.h"
 #include "behavior_tree_ros/ExchangeInfoAction.h"
 
 namespace BT_ROS
@@ -88,77 +86,6 @@ namespace BT_ROS
 
             std::vector<std::string> exchange_info_topic_msgs_;
     }; 
-    
-    class RosHandShakeData final
-    {
-        public:
-            RosHandShakeData();
-            ~RosHandShakeData() = default;
-
-        private:
-            void ThreeWayHandShakeDataTopicCallbackClient(const behavior_tree_ros::HandShakeData& _topic_msg);
-            void ThreeWayHandShakeDataTopicCallbackServer(const behavior_tree_ros::HandShakeData& _topic_msg);
-
-            void ThreeWayHandShakeDataActionCallback(const behavior_tree_ros::PerformHandShakeDataGoalConstPtr& _goal_msg);
-            void ThreeWayHandshakeDataServer(const behavior_tree_ros::PerformHandShakeDataGoalConstPtr& _goal_msg);
-            void ThreeWayHandshakeDataClient(const behavior_tree_ros::PerformHandShakeDataGoalConstPtr& _goal_msg);
-            void ThreeWayHandShakeDataActionPreemptCallback();
-            void PublishSmsCallback(const ros::TimerEvent& ev);
-
-        private:
-            ros::NodeHandle public_node_handle_;
-            ros::NodeHandle private_node_handle_ { "~" };
-
-            ros::Publisher  sync_publisher_;
-            ros::Subscriber sync_subscriber_;
-
-            actionlib::SimpleActionServer<behavior_tree_ros::PerformHandShakeDataAction> handshakedata_action_server_;
-            behavior_tree_ros::PerformHandShakeDataResult handshakedata_result_;
-
-            std::atomic<bool> sync_received_ {false};
-            std::atomic<bool> ack_received_ {false};
-            std::atomic<bool> action_cancelled_ {false};
-
-            int16_t my_seq_id_{0};
-            std::string data_to_send;
-            std::string data_rx;
-            std::string handshake_mode_;
-
-            ros::Timer pub_timer_;
-
-            double pub_period_s_ {1.0};
-
-            behavior_tree_ros::HandShakeData  msg_to_send_;
-    }; // class RosHandShake
-    // class RosExchangeInfo
-
-    // class RosExchangeImage final
-    // {
-    //     public:
-    //         RosExchangeImage();
-    //         ~RosExchangeImage() = default;
-
-    //     private:
-    //         void ExchangeImageTopicCallback(const std_msgs::StringConstPtr& _topic_msg);
-
-    //         void ExchangeImageActionCallback(const behavior_tree_ros::ExchangeImageGoalConstPtr& _goal_msg);
-    //         void ExchangeImageActionPreemptCallback();
-
-    //     private:
-    //         ros::NodeHandle public_node_handle_;
-    //         ros::NodeHandle private_node_handle_ { "~" };
-
-    //         ros::Publisher  send_image_publisher_;
-    //         ros::Subscriber get_image_subscriber_;
-
-    //         actionlib::SimpleActionServer<behavior_tree_ros::ExchangeImageAction> exchange_image_action_server_;
-    //         behavior_tree_ros::ExchangeImageResult exhange_image_action_result_;
-
-    //         std::atomic<bool> new_exhange_image_topic_msg_ { false };
-    //         std::mutex exhange_image_mutex_;
-
-    //         std::vector<behavior_tree_ros::Image> exhange_image_topic_msgs_;
-    // }; // class RosExchangeImage
 
 } // namespace BT_ROS
 
