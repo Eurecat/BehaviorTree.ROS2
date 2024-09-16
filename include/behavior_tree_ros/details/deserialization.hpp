e5c616dd370a42c11900f1c7a193378bc59445c5
Devis Dal Moro <devis.dalmoro@eurecat.org>
2024-09-16 09:22:00 +0200

    Issue with json deserialization in signed/unsigned

--- include/behavior_tree_ros/details/deserialization.hpp
+++ include/behavior_tree_ros/details/deserialization.hpp
@@ -15,7 +15,7 @@ namespace deserialization
     using Json = nlohmann::json;
     using DeserializeFieldFunction = std::function<void(BT::ActionNodeBase&, const std::string&, const Json&)>;
 
-    void deserializeFieldToSignedInt(BT::ActionNodeBase& _node, const std::string& _port, const Json& _field)
+    void deserializeFieldFromSignedInt(BT::ActionNodeBase& _node, const std::string& _port, const Json& _field)
     {
         const auto port_info = _node.getPortInfo(_port);
         
@@ -47,7 +47,7 @@ namespace deserialization
     
     }
 
-    void deserializeFieldToUnsignedInt(BT::ActionNodeBase& _node, const std::string& _port, const Json& _field)
+    void deserializeFieldFromUnsignedInt(BT::ActionNodeBase& _node, const std::string& _port, const Json& _field)
     {
         const auto port_info = _node.getPortInfo(_port);
         
@@ -57,13 +57,27 @@ namespace deserialization
         {
             const std::type_info* type_info = port_info.value().type();
 
-            if(*type_info == typeid(unsigned short)) // shall match uint16_t
-                _node.setOutput<unsigned short>(_port, _field.get<unsigned short>());
-            else if(*type_info == typeid(unsigned int)) // shall match uint32_t
-                _node.setOutput<unsigned int>(_port, _field.get<unsigned int>());
-            else if(*type_info == typeid(unsigned long)) // shall match uint64_t and size_t
-                _node.setOutput<unsigned long>(_port, _field.get<unsigned long>());
-             else 
+            // Retrieve the value as uint64_t (maximum unsigned size)
+            uint64_t value = _field.get<uint64_t>();
+            
+            // Check for unsigned types
+            if (*type_info == typeid(unsigned short) && value <= std::numeric_limits<unsigned short>::max()) // shall match uint16_t
+                _node.setOutput<unsigned short>(_port, static_cast<unsigned short>(value));
+            else if (*type_info == typeid(unsigned int) && value <= std::numeric_limits<unsigned int>::max()) // shall match uint32_t
+                    _node.setOutput<unsigned int>(_port, static_cast<unsigned int>(value));
+            else if (*type_info == typeid(unsigned long) && value <= std::numeric_limits<unsigned long>::max()) // shall match uint64_t
+                    _node.setOutput<unsigned long>(_port, static_cast<unsigned long>(value));
+            
+            // Check for signed types
+            else if (*type_info == typeid(short) && value <= static_cast<uint64_t>(std::numeric_limits<short>::max())) // signed short
+                    _node.setOutput<short>(_port, static_cast<short>(value));
+            else if (*type_info == typeid(int) && value <= static_cast<uint64_t>(std::numeric_limits<int>::max())) // signed int
+                    _node.setOutput<int>(_port, static_cast<int>(value));
+
+            else if (*type_info == typeid(long) && value <= static_cast<uint64_t>(std::numeric_limits<long>::max())) // signed long
+                    _node.setOutput<long>(_port, static_cast<long>(value));
+
+            else 
                 match_not_found = true; //perform setOutput to max int, i.e. uint64
         }
         else
@@ -108,8 +122,8 @@ namespace deserialization
     {
         { Json::value_t::boolean,         [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToBool(_node, _port, _field);        }},
         { Json::value_t::string,          [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToString(_node, _port, _field); }},
-        { Json::value_t::number_integer,  [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToSignedInt(_node, _port, _field);     }},
-        { Json::value_t::number_unsigned, [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToUnsignedInt(_node, _port, _field);    }},
+        { Json::value_t::number_integer,  [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldFromSignedInt(_node, _port, _field);     }},
+        { Json::value_t::number_unsigned, [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldFromUnsignedInt(_node, _port, _field);    }},
         { Json::value_t::number_float,    [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToDouble(_node, _port, _field);      }},
         { Json::value_t::object,          [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToJson(_node, _port, _field);        }},
         { Json::value_t::array,           [] (auto& _node, const auto& _port, const auto& _field) { deserializeFieldToJson(_node, _port, _field);        }},
