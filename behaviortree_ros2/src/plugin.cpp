19e957da07e546b1cd3a16ffd8ad092e341509f8
Jonathan Cacace <jonathan.cacace@eurecat.org>
2025-10-20 23:18:28 +0200

    bugfix on nested messages

--- behaviortree_ros2/src/plugin.cpp
+++ behaviortree_ros2/src/plugin.cpp
@@ -19,6 +19,7 @@
 #include <std_msgs/msg/float32.hpp>
 #include <std_msgs/msg/float64.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
 #include <std_srvs/srv/empty.hpp>
 #include <std_srvs/srv/set_bool.hpp>
@@ -30,11 +31,64 @@
 #include "btcpp_ros2_interfaces/action/execute_tree.hpp"
 #include "btcpp_ros2_interfaces/msg/custom_msg.hpp"
 
+// Simple Action that updates an instance of Position2D in the blackboard
+class SayHi : public BT::StatefulActionNode
+{
+public:
+  SayHi(const std::string& name, const BT::NodeConfig& config)
+    : BT::StatefulActionNode(name, config)
+  {}
+
+  static BT::PortsList providedPorts()
+  {
+    return { BT::InputPort<std::string>("person_name") };
+  }
+
+  BT::NodeStatus onStart() override
+  {
+    const auto name = getInput<std::string>("person_name");
+    if(!name)
+      return BT::NodeStatus::FAILURE;
+
+    person_name_ = name.value();
+    counter_ = 0;
+
+    std::cout << "Starting to say hi to " << person_name_ << "..." << std::endl;
+    return BT::NodeStatus::RUNNING;
+  }
+
+  BT::NodeStatus onRunning() override
+  {
+    if (counter_ < 3)
+    {
+      std::cout << "Hi " << person_name_ << "! (" << counter_+1 << "/3)" << std::endl;
+      counter_++;
+      return BT::NodeStatus::RUNNING;
+    }
+    else
+    {
+      std::cout << "Finished saying hi to " << person_name_ << "!" << std::endl;
+      return BT::NodeStatus::SUCCESS;
+    }
+  }
+
+  void onHalted() override
+  {
+    std::cout << "SayHi halted." << std::endl;
+  }
+
+private:
+  std::string person_name_;
+  int counter_ = 0;
+};
 
 using namespace BT;
 
 BT_REGISTER_ROS_NODES(factory, params)
 {
+
+    factory.registerNodeType<SayHi>("SayHi");
+
     //LOGS
     factory.registerNodeType<DebugLog>("DebugLog");
     factory.registerNodeType<InfoLog>("InfoLog");
@@ -62,6 +116,8 @@ BT_REGISTER_ROS_NODES(factory, params)
     factory.registerNodeType<AutoSerSubscriber<std_msgs::msg::Float32>>("MonitorAutoStdFloat",params);
     factory.registerNodeType<AutoSerSubscriber<std_msgs::msg::Float64>>("MonitorAutoStdDouble",params);
     factory.registerNodeType<AutoSerSubscriber<std_msgs::msg::String>>("MonitorAutoStdString",params);
+    factory.registerNodeType<AutoSerSubscriber<geometry_msgs::msg::PoseStamped>>("MonitorAutoPoseStamped",params);
+    factory.registerNodeType<JsonSerSubscriber<geometry_msgs::msg::PoseStamped>>("MonitorJsonPoseStamped",params);
 
     // Possible variant of PRIMITIVE SUBSCRIBER WITH JSON Serialization
     factory.registerNodeType<JsonSerSubscriber<std_msgs::msg::Int32>>("MonitorJsonStdInt",params);
@@ -80,7 +136,8 @@ BT_REGISTER_ROS_NODES(factory, params)
     factory.registerNodeType<AutoDesPublisher<std_msgs::msg::Float32>>("PublishAutoStdFloat",params);
     factory.registerNodeType<AutoDesPublisher<std_msgs::msg::Float64>>("PublishAutoStdDouble",params);
     factory.registerNodeType<AutoDesPublisher<std_msgs::msg::String>>("PublishAutoStdString",params);
-
+    factory.registerNodeType<AutoDesPublisher<geometry_msgs::msg::PoseStamped>>("PublishAutoPoseStamped",params);
+    
     //PRIMITIVE SERVICES
     factory.registerNodeType<AutoDesJsonSerServiceClient<std_srvs::srv::Empty>>("ServiceAutoCallJsonEmpty",params);
     factory.registerNodeType<AutoDesJsonSerServiceClient<std_srvs::srv::SetBool>>("ServiceAutoCallJsonBool",params);
@@ -96,5 +153,6 @@ BT_REGISTER_ROS_NODES(factory, params)
     factory.registerNodeType<AutoDesPublisher<std_msgs::msg::Int8MultiArray>>("PublishStdMultiArray",params);
     factory.registerNodeType<SmartJsonSerSubscriber<btcpp_ros2_interfaces::msg::NodeStatus>>("MonitorSmartJsonNodeStatus",params);
     factory.registerNodeType<SmartJsonSerSubscriber<btcpp_ros2_interfaces::msg::CustomMsg>>("MonitorSmartJsonCustomMsg",params);
+
 
 };
